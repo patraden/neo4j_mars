5bdb8dc1fdc39b7855caafc2e06d4c8755aee49a
patraden <denis.patrakhin@gmail.com>
2020-05-23 11:31:42 +0100

    new commit

--- application1/wsgi_test.py
+++ application1/wsgi_test.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Hello World!'
+    output = bytes('Hello World!','utf-8')
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
