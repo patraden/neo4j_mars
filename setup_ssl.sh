ed6a45c22e3e2df5034f7eddc5f8718ea4e5cf10
patraden <denis.patrakhin@gmail.com>
2020-06-01 08:12:50 +0100

    new commit

--- setup_ssl.sh
+++ setup_ssl.sh
@@ -18,8 +18,9 @@ set -x
 
 set -eo pipefail
 
-# Generating and setting private and public keys from Mars AD *.psx CA certificate file
-# CA password for .psx file should be given as parameter to docker build --build-arg CERRTIFICATE_PASSWORD='password'
+# Generating and setting private and public keys from Mars AD *.psx CA certificate file.
+# Password for .psx file should be provided as argument to docker build: --build-arg CERRTIFICATE_PASSWORD='password'
+# Finally modifying permissions for resulting certificate files to be accessible to root only
 
 openssl pkcs12 -in /tmp/$CERTIFICATE_NAME.pfx -nocerts -nodes -out /tmp/tmp.key -passin pass:$CERTIFICATE_PASSWORD
 openssl rsa -in /tmp/tmp.key -out /etc/ssl/private/$CERTIFICATE_NAME.key #removs password from private key
@@ -29,10 +30,17 @@ chmod 0600 /etc/ssl/certs/$CERTIFICATE_NAME.crt
 chmod 0600 /etc/ssl/private/$CERTIFICATE_NAME.key
 
 # Configure and enable mod_wsgi and ssl with apache2
-# Enabling relevant virtual hosts configurations
 
 a2enmod ssl
+
+# Disable default virtual hosts configurations and enabling only relevant configurations
+
 cd /etc/apache2/sites-enabled/
 a2dissite *
+
 cd /etc/apache2/sites-available/
 find . -type f -and -name "*.apps.wsgi.conf" -exec a2ensite {} \;
+
+# Clean up the temporary files
+
+rm -r /tmp/*
